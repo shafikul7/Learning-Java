c308d4965f141f26692552e2489f4ae21b70f656
Md.Shafikul Islam <63946398+Shafikul-islam1@users.noreply.github.com>
2021-03-08 19:43:57 +0600

    Add files via upload

---
+++ Java-Problem-solving/Java Stdin and Stdout II.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        double y = sc.nextDouble();
+        sc.nextLine();
+        String s = sc.nextLine();
+        // Write your code here.
+        // String name = "dog";
+
+        System.out.println("String: "+s);
+        System.out.println("Double: "+y);
+        System.out.println("Int: " +x);
+    }
+}
+
