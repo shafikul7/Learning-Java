d60c21c1207c8151b88151ca55a0235ef8366dba
Md.Shafikul Islam <63946398+Shafikul-islam1@users.noreply.github.com>
2021-03-09 20:44:33 +0600

    Update Loop.java

--- Java-code/Loop-5/Loop.java
+++ Java-code/Loop-5/Loop.java
@@ -1,61 +1,61 @@
-// import java.util.Scanner;  // Import the Scanner class
-// public class Loop{
-//     public static void main(String[] args) {
-//         String name ="Md.Shafikul islam";
+import java.util.Scanner;  // Import the Scanner class
+public class Loop{
+    public static void main(String[] args) {
+        String name ="Md.Shafikul islam";
 
-//         for(int i=0; i<10; i++){
+        for(int i=0; i<10; i++){
 
-//             System.out.println("My name is : " +i+ name);
-//         }
-
-        
-//     }
-// }
-// import java.util.Scanner;  // Import the Scanner class
-// public class Loop{
-//     public static void main(String[] args) {
-//         String name ="Md.Shafikul islam";
-
-//         for(int i=1; i<100; i++){
-//              if(i%10==0){
-//             System.out.println("My name is : " +i+ name);
-        
-//         } 
-//     }
+            System.out.println("My name is : " +i+ name);
+        }
 
         
-//     }
-// }
+    }
+}
+import java.util.Scanner;  // Import the Scanner class
+public class Loop{
+    public static void main(String[] args) {
+        String name ="Md.Shafikul islam";
 
-// import java.util.Scanner;  // Import the Scanner class
-// public class Loop{
-//     public static void main(String[] args) {
-//         int n=10;
-//         int sum=0;
-
-//         for(int i=1; i<=n; i++){
-//             sum+=i;
+        for(int i=1; i<100; i++){
+             if(i%10==0){
+            System.out.println("My name is : " +i+ name);
         
-//         } 
-//         System.out.println(sum);
-//     }
-// }
+        } 
+    }
 
-// import java.util.Scanner;  // Import the Scanner class
-// public class Loop{
-//     public static void main(String[] args) throws InterruptedException {
-//     int limit;
-//         Scanner sr = new Scanner(System.in); 
-//         System.out.println("Enter the Acoumt of second");
-//         limit = sr.nextInt();
-//         for(int i=limit; i==0;i++){
-//             System.out.println("Time limit" + i);
-//             Thread.sleep(1000);
+        
+    }
+}
 
-//         }
-//         System.out.println("Time count complit" );
-//     }
-// }
+import java.util.Scanner;  // Import the Scanner class
+public class Loop{
+    public static void main(String[] args) {
+        int n=10;
+        int sum=0;
+
+        for(int i=1; i<=n; i++){
+            sum+=i;
+        
+        } 
+        System.out.println(sum);
+    }
+}
+
+import java.util.Scanner;  // Import the Scanner class
+public class Loop{
+    public static void main(String[] args) throws InterruptedException {
+    int limit;
+        Scanner sr = new Scanner(System.in); 
+        System.out.println("Enter the Acoumt of second");
+        limit = sr.nextInt();
+        for(int i=limit; i==0;i++){
+            System.out.println("Time limit" + i);
+            Thread.sleep(1000);
+
+        }
+        System.out.println("Time count complit" );
+    }
+}
 
 import java.util.*;
   // Import the Scanner class
