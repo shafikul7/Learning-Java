89c3fb7f6703a52babb32081dc5e7c8407c3fccf
Md.Shafikul Islam <63946398+Shafikul-islam1@users.noreply.github.com>
2021-03-11 00:09:11 +0600

    Update Java Loops I.java

--- Java-Problem-solving/Java Loops I.java
+++ Java-Problem-solving/Java Loops I.java
@@ -1,9 +1,3 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
 public class Solution {
 
     public static void main(String[] args) {
