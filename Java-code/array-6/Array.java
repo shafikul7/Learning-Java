122189c6a9e1db5027c037f8ece8fd52ee863d4c
Md.Shafikul Islam <63946398+Shafikul-islam1@users.noreply.github.com>
2021-03-10 15:49:18 +0600

    Add files via upload

---
+++ Java-code/array-6/Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] args) {
+        
+        int [] arr= {11, 22, 33, 44};
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + "=>" + cars[i]);
+        }
+
+    }
+}
