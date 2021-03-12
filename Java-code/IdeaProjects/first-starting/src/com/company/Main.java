04e20fcd7f0bb3feab9c0797bd5b609ddc8785b3
Md.Shafikul Islam <63946398+Shafikul-islam1@users.noreply.github.com>
2021-03-12 21:20:15 +0600

    Add files via upload

---
+++ Java-code/IdeaProjects/first-starting/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world");
+        System.out.println("Md.Shafikul islam 77");
+    }
+}
