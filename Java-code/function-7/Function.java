490b58df1e640c2289574a707a716f429c043038
Md.Shafikul Islam <63946398+Shafikul-islam1@users.noreply.github.com>
2021-03-12 18:16:35 +0600

    Add files via upload

---
+++ Java-code/function-7/Function.java
@@ -0,0 +1,15 @@
+public class Function {
+    public static void main(String[] args) {
+        
+        
+    }
+    public static void add(int a , int b , int c , int d){
+        System.out.println(a+b+c+d);
+    }
+    public static double celcius(double farenheit){
+
+    }
+       
+    }
+   
+}
